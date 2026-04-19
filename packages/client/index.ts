f09ac1033c62343b9dc5b2c56633fb0f0c6bfd8d
Fernando Dantas <fernandodantas1996@gmail.com>
2026-04-18 21:10:42 -0300

    feat: added minimal websockets server/client

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { WebSocket } from 'ws';
+
+console.log('Client is running')
+
+const ws = new WebSocket('ws://localhost:8080');
+
+ws.on('error', console.error);
+
+ws.on('open', function() {
+    ws.send('something');
+});
+
+ws.on('message', function message(_) {
+})
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { WebSocketServer } from 'ws';
+
+console.log('Server is running');
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error)
+
+    ws.on('message', function message(data) {
+        ws.send('[server] ack -> ' + data)
+    });
+});
